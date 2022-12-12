11fcc2538288adc334ae8ed4ac864616442c0dfd
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-12-12 10:57:05 +0200

    ..

--- src/components/portofolio/projects.js
+++ src/components/portofolio/projects.js
@@ -1,165 +1,165 @@
 export const projects = [
-  {
-    id: 1,
-    name: "PS22-space-tourism-html-scss-js",
-    demoLink: "https://mkhaled-farag.netlify.app/",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-space-tourism-html-scss-js",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-space-tourism-html-scss-js/master/image.png",
-  },
-  {
-    id: 2,
-    name: "PS22-expenses-tracker-react-reduxtk",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-expenses-tracker-react-reduxtk/",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-expenses-tracker-react-reduxtk",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-expenses-tracker-react-reduxtk/master/image.png",
-  },
-  {
-    id: 3,
-    name: "PS22-weather-app-react-reduxtk",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-weather-app-react-reduxtk",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-weather-app-react-reduxtk",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-weather-app-react-reduxtk/master/image.png",
-  },
-  {
-    id: 4,
-    name: "PS22-profile-card-component-html-css",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-profile-card-component-html-css",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-profile-card-component-html-css",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-profile-card-component-html-css/master/image.png",
-  },
-  {
-    id: 5,
-    name: "PS22-typing-speed-game-html-js-css",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-typing-speed-game-html-js-css",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-typing-speed-game-html-js-css",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-typing-speed-game-html-js-css/master/image.png",
-  },
-  {
-    id: 6,
-    name: "PS22-tic-tac-toe-react-css",
-    demoLink: "https://xokhaled.netlify.app/",
-    repoLink: "https://github.com/mohammedkhaled15/PS22-tic-tac-toe-react-css",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-tic-tac-toe-react-css/master/image.png",
-  },
-  {
-    id: 7,
-    name: "PS22-memory-cards-html-css-js ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-memory-cards-html-css-js",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-memory-cards-html-css-js ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-memory-cards-html-css-js /master/image.png",
-  },
-  {
-    id: 8,
-    name: "PS22-leon-agency-html-css  ",
-    demoLink: "https://mohammedkhaled15.github.io/PS22-leon-agency-html-css ",
-    repoLink: "https://github.com/mohammedkhaled15/PS22-leon-agency-html-css  ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-leon-agency-html-css  /master/image.png",
-  },
-  {
-    id: 9,
-    name: "PS22-kasper-html-css  ",
-    demoLink: "https://mohammedkhaled15.github.io/PS22-kasper-html-css ",
-    repoLink: "https://github.com/mohammedkhaled15/PS22-kasper-html-css  ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-kasper-html-css  /master/image.png",
-  },
-  {
-    id: 10,
-    name: "PS22-fancy-agency-html-css-js  ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-fancy-agency-html-css-js ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-fancy-agency-html-css-js  ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-fancy-agency-html-css-js/master/image.png",
-  },
-  {
-    id: 11,
-    name: "PS22-game-battle-js-oop  ",
-    demoLink: "https://mohammedkhaled15.github.io/PS22-game-battle-js-oop ",
-    repoLink: "https://github.com/mohammedkhaled15/PS22-game-battle-js-oop  ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-game-battle-js-oop/master/image.png",
-  },
-  {
-    id: 12,
-    name: "PS22-fylo-data-storage-html-css   ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-fylo-data-storage-html-css  ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-fylo-data-storage-html-css   ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-fylo-data-storage-html-css /master/image.png",
-  },
-  {
-    id: 13,
-    name: "PS22-order-summary-card-html-css   ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-order-summary-card-html-css  ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-order-summary-card-html-css   ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-order-summary-card-html-css /master/image.png",
-  },
-  {
-    id: 14,
-    name: "PS22-shoes-ecommerce-react-tailwind   ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-shoes-ecommerce-react-tailwind  ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-shoes-ecommerce-react-tailwind   ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-shoes-ecommerce-react-tailwind/master/image.png",
-  },
-  {
-    id: 15,
-    name: "PS22-add-remove-classes-html-js   ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-add-remove-classes-html-js  ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-add-remove-classes-html-js   ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-add-remove-classes-html-js/master/image.png",
-  },
-  {
-    id: 16,
-    name: "PS22-agency-website1-bootstrap-html-js   ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-agency-website1-bootstrap-html-js  ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-agency-website1-bootstrap-html-js   ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-agency-website1-bootstrap-html-js/master/image.png",
-  },
-  {
-    id: 17,
-    name: "PS22-calculator-html-scss-js   ",
-    demoLink:
-      "https://mohammedkhaled15.github.io/PS22-calculator-html-scss-js  ",
-    repoLink:
-      "https://github.com/mohammedkhaled15/PS22-calculator-html-scss-js   ",
-    imgUrl:
-      "https://raw.githubusercontent.com/mohammedkhaled15/PS22-calculator-html-scss-js/master/image.png",
-  },
+  // {
+  //   id: 1,
+  //   name: "PS22-space-tourism-html-scss-js",
+  //   demoLink: "https://mkhaled-farag.netlify.app/",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-space-tourism-html-scss-js",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-space-tourism-html-scss-js/master/image.png",
+  // },
+  // {
+  //   id: 2,
+  //   name: "PS22-expenses-tracker-react-reduxtk",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-expenses-tracker-react-reduxtk/",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-expenses-tracker-react-reduxtk",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-expenses-tracker-react-reduxtk/master/image.png",
+  // },
+  // {
+  //   id: 3,
+  //   name: "PS22-weather-app-react-reduxtk",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-weather-app-react-reduxtk",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-weather-app-react-reduxtk",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-weather-app-react-reduxtk/master/image.png",
+  // },
+  // {
+  //   id: 4,
+  //   name: "PS22-profile-card-component-html-css",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-profile-card-component-html-css",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-profile-card-component-html-css",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-profile-card-component-html-css/master/image.png",
+  // },
+  // {
+  //   id: 5,
+  //   name: "PS22-typing-speed-game-html-js-css",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-typing-speed-game-html-js-css",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-typing-speed-game-html-js-css",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-typing-speed-game-html-js-css/master/image.png",
+  // },
+  // {
+  //   id: 6,
+  //   name: "PS22-tic-tac-toe-react-css",
+  //   demoLink: "https://xokhaled.netlify.app/",
+  //   repoLink: "https://github.com/mohammedkhaled15/PS22-tic-tac-toe-react-css",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-tic-tac-toe-react-css/master/image.png",
+  // },
+  // {
+  //   id: 7,
+  //   name: "PS22-memory-cards-html-css-js ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-memory-cards-html-css-js",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-memory-cards-html-css-js ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-memory-cards-html-css-js /master/image.png",
+  // },
+  // {
+  //   id: 8,
+  //   name: "PS22-leon-agency-html-css  ",
+  //   demoLink: "https://mohammedkhaled15.github.io/PS22-leon-agency-html-css ",
+  //   repoLink: "https://github.com/mohammedkhaled15/PS22-leon-agency-html-css  ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-leon-agency-html-css  /master/image.png",
+  // },
+  // {
+  //   id: 9,
+  //   name: "PS22-kasper-html-css  ",
+  //   demoLink: "https://mohammedkhaled15.github.io/PS22-kasper-html-css ",
+  //   repoLink: "https://github.com/mohammedkhaled15/PS22-kasper-html-css  ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-kasper-html-css  /master/image.png",
+  // },
+  // {
+  //   id: 10,
+  //   name: "PS22-fancy-agency-html-css-js  ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-fancy-agency-html-css-js ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-fancy-agency-html-css-js  ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-fancy-agency-html-css-js/master/image.png",
+  // },
+  // {
+  //   id: 11,
+  //   name: "PS22-game-battle-js-oop  ",
+  //   demoLink: "https://mohammedkhaled15.github.io/PS22-game-battle-js-oop ",
+  //   repoLink: "https://github.com/mohammedkhaled15/PS22-game-battle-js-oop  ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-game-battle-js-oop/master/image.png",
+  // },
+  // {
+  //   id: 12,
+  //   name: "PS22-fylo-data-storage-html-css   ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-fylo-data-storage-html-css  ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-fylo-data-storage-html-css   ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-fylo-data-storage-html-css /master/image.png",
+  // },
+  // {
+  //   id: 13,
+  //   name: "PS22-order-summary-card-html-css   ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-order-summary-card-html-css  ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-order-summary-card-html-css   ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-order-summary-card-html-css /master/image.png",
+  // },
+  // {
+  //   id: 14,
+  //   name: "PS22-shoes-ecommerce-react-tailwind   ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-shoes-ecommerce-react-tailwind  ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-shoes-ecommerce-react-tailwind   ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-shoes-ecommerce-react-tailwind/master/image.png",
+  // },
+  // {
+  //   id: 15,
+  //   name: "PS22-add-remove-classes-html-js   ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-add-remove-classes-html-js  ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-add-remove-classes-html-js   ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-add-remove-classes-html-js/master/image.png",
+  // },
+  // {
+  //   id: 16,
+  //   name: "PS22-agency-website1-bootstrap-html-js   ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-agency-website1-bootstrap-html-js  ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-agency-website1-bootstrap-html-js   ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-agency-website1-bootstrap-html-js/master/image.png",
+  // },
+  // {
+  //   id: 17,
+  //   name: "PS22-calculator-html-scss-js   ",
+  //   demoLink:
+  //     "https://mohammedkhaled15.github.io/PS22-calculator-html-scss-js  ",
+  //   repoLink:
+  //     "https://github.com/mohammedkhaled15/PS22-calculator-html-scss-js   ",
+  //   imgUrl:
+  //     "https://raw.githubusercontent.com/mohammedkhaled15/PS22-calculator-html-scss-js/master/image.png",
+  // },
   {
     id: 18,
     name: "PS22-adding-and-delete-tasks-js-css    ",
